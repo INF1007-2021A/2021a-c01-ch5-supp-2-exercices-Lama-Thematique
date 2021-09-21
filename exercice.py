457270ea67abe2478a2a9d38babe1d0d04b25512
Lama Thématique <gaetan.butault@polymtl.ca>
2021-09-21 16:44:32 -0400

    ex done

--- exercice.py
+++ exercice.py
@@ -4,21 +4,42 @@
 import math
 
 def get_num_letters(text):
-	return 0
+	co = 0
+	for c in text:
+		if (c.isalnum()):
+			co+=1
+	return co
 
 def get_word_length_histogram(text):
-	return [0]
+	maxi = 0
+	li = [0]*(len(text)+1)
+	for mot in text.split():
+		li[get_num_letters(mot)] += 1
+		maxi = max(maxi,get_num_letters(mot))
+	return li[:maxi+1]
 
 def format_histogram(histogram):
 	ROW_CHAR = "*"
-
-	return ""
+	result = ""
+	maxlen = len(histogram)
+	for i in range(1,len(histogram)):
+		result += " "*(len(str(maxlen))-len(str(i)))+str(i)+" "+ROW_CHAR*histogram[i]+"\n"
+	return result[:-1]
 
 def format_horizontal_histogram(histogram):
 	BLOCK_CHAR = "|"
 	LINE_CHAR = "¯"
-
-	return ""
+	result = ""
+	for i in range(max(histogram),0,-1):
+		for lenght in histogram[1:]:
+			if(i <= lenght):
+				result+= BLOCK_CHAR
+			else :
+				result+= " "
+		result+="\n"
+	result+= LINE_CHAR*(len(histogram)-1)
+		
+	return result
 
 
 if __name__ == "__main__":
